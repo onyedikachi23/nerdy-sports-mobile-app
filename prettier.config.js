6607c2e278f8d187fcfce31de27c4a74b32442b5
onyedkichi23 <ozohenrywk@gmail.com>
2025-06-26 01:33:45 +0100

    chore: update dependencies and add Prettier configuration
    - Added `lucide-react-native` version 0.523.0 to package.json.
    - Added `prettier` version 3.6.1 and `prettier-plugin-tailwindcss` version 0.6.13 to package.json.
    - Created a new Prettier configuration file with custom settings.

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// prettier.config.js
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "tailwind.config.js",
+  endOfLine: "auto",
+  bracketSameLine: true,
+  useTabs: true,
+  tabWidth: 4,
+  htmlWhitespaceSensitivity: "strict",
+  insertPragma: true,
+};
