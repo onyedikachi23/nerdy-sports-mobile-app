fdf3994125effaac60ce4998b01d31606e82eb30
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-02 15:54:19 +0100

    refactor: inline padding style in BoxScreen component and export BOX_SCREEN_PADDING constant for consistent spacing

--- src/components/ui-extended/box.tsx
+++ src/components/ui-extended/box.tsx
@@ -4,11 +4,16 @@ import type React from "react";
 import { Box } from "../ui/box";
 import { cn } from "@/lib/utils";
 import { type AsChildProps, PropSlot } from "./slot";
+import { StyleSheet } from "react-native";
 
 type BoxScreenProps = AsChildProps<React.ComponentProps<typeof Box>>;
 
+/**The padding value used in `style.padding` for {@link BoxScreen} */
+const BOX_SCREEN_PADDING = 28;
+
 const BoxScreen: React.FC<BoxScreenProps> = ({
 	className,
+	style,
 	asChild,
 	...props
 }) => {
@@ -16,10 +21,16 @@ const BoxScreen: React.FC<BoxScreenProps> = ({
 	return (
 		<Comp
 			{...props}
+			style={StyleSheet.flatten([
+				{
+					padding: BOX_SCREEN_PADDING,
+				},
+				style,
+			])}
 			className={cn("flex-1 gap-4 bg-brand-background p-8", className)}
 		/>
 	);
 };
 
-export { BoxScreen };
+export { BoxScreen, BOX_SCREEN_PADDING };
 export type { BoxScreenProps };
