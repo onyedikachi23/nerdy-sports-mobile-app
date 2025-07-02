970b53d479edc32269cb830f469e18886e1e86ed
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-02 15:57:38 +0100

    refactor: enhance AuthButton component by adding variant and size props for better customization

--- src/components/app/auth/auth-button.tsx
+++ src/components/app/auth/auth-button.tsx
@@ -3,23 +3,38 @@
 /** @format */
 
 import { Button, ButtonText } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 import type React from "react";
 
-interface AuthButtonProps {
+interface AuthButtonProps
+	extends SafeOmit<React.ComponentProps<typeof Button>, "action"> {
 	authAction: "signup" | "login";
 	children: string;
+	className?: string;
 }
 
-export const AuthButton: React.FC<AuthButtonProps> = ({
+const AuthButton: React.FC<AuthButtonProps> = ({
 	authAction,
 	children,
+	variant = "solid",
+	size = "xl",
+	className,
+	...props
 }) => (
 	<Button
+		{...props}
 		action={authAction === "signup" ? "secondary" : "primary"}
-		variant={"solid"}
-		size={"xl"}
-		className="rounded-full">
-		<ButtonText>{children}</ButtonText>
+		variant={variant}
+		size={size}
+		className={cn(
+			"rounded-full",
+			authAction === "login" && "border",
+			className,
+		)}>
+		<ButtonText className="flex-1 text-center">{children}</ButtonText>
 	</Button>
 );
+
+export { AuthButton };
+export type { AuthButtonProps };
