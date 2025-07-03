445e09a1ac3f8b81748ed5e95723c44eb2a09b93
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-03 14:39:45 +0100

    refactor: enhance rgbToHex function to accept RGBA object and validate alpha value

--- src/hooks/use-themed-color.ts
+++ src/hooks/use-themed-color.ts
@@ -10,14 +10,35 @@ import {
 import { useAppColorScheme } from "./use-color-scheme";
 import React from "react";
 
-const rgbToHex = (r: number, g: number, b: number) => {
+interface RGBA {
+	r: number;
+	g: number;
+	b: number;
+	/**0-100 opacity */
+	a?: number;
+}
+
+const rgbToHex = ({ r, g, b, a }: RGBA) => {
 	const HEX_RADIX = 16;
 	const HEX_PREFIX = "#";
 	const toHex = (c: number) => {
 		const hex = Math.round(c).toString(HEX_RADIX);
 		return hex.length === 1 ? `0${hex}` : hex;
 	};
-	return `${HEX_PREFIX}${toHex(r)}${toHex(g)}${toHex(b)}` as const;
+
+	let hexColor = `${HEX_PREFIX}${toHex(r)}${toHex(g)}${toHex(b)}` as const;
+
+	if (a) {
+		if (a < 0 || a > 100) {
+			throw new Error(
+				`[rgbToHex]: Alpha "a" must be a value between 0 and 100. Instead got: "${a}".`,
+			);
+		}
+
+		const alpha255 = (a / 100) * 255;
+		hexColor = `${hexColor}${toHex(alpha255)}`;
+	}
+	return hexColor;
 };
 
 type StripColorPrefix<T extends ColorKey> =
@@ -26,6 +47,34 @@ type StripColorPrefix<T extends ColorKey> =
 		: never;
 
 type ThemeColorName = StripColorPrefix<ThemeColorKey>;
+
+type TailwindOpacity =
+	| 0
+	| 5
+	| 10
+	| 20
+	| 25
+	| 30
+	| 40
+	| 50
+	| 60
+	| 70
+	| 75
+	| 80
+	| 90
+	| 95
+	| 100;
+
+const OPACITY_SEPERATOR = "/";
+
+type ThemedColorNameWithOpacity =
+	`${ThemeColorName}${typeof OPACITY_SEPERATOR}${TailwindOpacity}`;
+
+/**
+ * Represents a themed color name, optionally including a Tailwind CSS opacity value.
+ * Examples: "primary-500", "primary-500/50"
+ */
+type ThemedColorReference = ThemeColorName | ThemedColorNameWithOpacity;
 
 type RGBArray<T extends ColorValue = ColorValue> =
 	T extends `${infer R extends number} ${infer G extends number} ${infer B extends number}`
@@ -43,16 +92,29 @@ const useThemedColor = () => {
 	const colorConfig = colorScheme === "dark" ? darkThemeMap : lightThemeMap;
 
 	const getHexColor = React.useCallback(
-		(colorName: ThemeColorName): string => {
+		(colorRef: ThemedColorReference): string => {
+			const parts = colorRef.split(OPACITY_SEPERATOR);
+
+			const colorName = parts[0] as ThemeColorName;
+			const opacity =
+				parts.length > 1 && typeof parts[1] === "string"
+					? parseFloat(parts[1])
+					: undefined;
+
 			const colorKey = `--color-${colorName}` satisfies ThemeColorKey;
-			const rgbArray = colorConfig[colorKey].split(" ").map(parseFloat); // parseInt gives weird results
+			const COLOR_VALUE_SEPERATOR = " ";
+			const rgbArray = colorConfig[colorKey]
+				.split(COLOR_VALUE_SEPERATOR)
+				.map(parseFloat);
 
 			if (!isValidRGBArray(rgbArray)) {
 				throw new Error(
 					`Unable to parse RGB values from specified colorName: "${colorName}"`,
 				);
 			}
-			return rgbToHex(...rgbArray);
+
+			const [r, g, b] = rgbArray;
+			return rgbToHex({ r, g, b, a: opacity });
 		},
 		[colorConfig],
 	);
@@ -61,4 +123,9 @@ const useThemedColor = () => {
 };
 
 export { useThemedColor };
-export type { ThemeColorName, StripColorPrefix };
+export type {
+	ThemeColorName,
+	ThemedColorNameWithOpacity,
+	ThemedColorReference,
+	StripColorPrefix,
+};
