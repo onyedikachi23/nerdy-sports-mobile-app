28ee282a04cd16c2a88a3f653b3a8dd7dacfe942
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-05 18:04:55 +0100

    feat: update AuthLayout to use ScrollView for improved scrolling and add background image

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,74 +1,45 @@
 /** @format */
 
-/** @format */
-
+import { BlurredGradientBg } from "@/components/app/auth/blurred-gradient-bg";
+import { AppLogoWithText } from "@/components/ui-common/app-logo";
 import {
-	AuthButton,
-	type AuthButtonProps,
-} from "@/components/app/auth/auth-button";
-import { BOX_SCREEN_PADDING, BoxScreen } from "@/components/ui-extended/box";
-import { Box } from "@/components/ui/box";
-import { cn } from "@/lib/utils";
-import {
-	TabList,
-	Tabs,
-	TabSlot,
-	TabTrigger,
-	type TabTriggerProps,
-} from "expo-router/ui";
+	BOX_SCREEN_PADDING,
+	BoxScreen,
+	Box,
+} from "@/components/ui-extended/box";
+import { Image } from "@/components/ui-extended/image";
+import { Slot } from "expo-router";
 import type React from "react";
 
-type TabButtonProps = Merge<
-	SafeOmit<TabTriggerProps, "name" | "href">,
-	AuthButtonProps
->;
+// was not imported from react-native because scrolling on touch gesture won't
+// work (maybe because of the complex positioning.)
+import { ScrollView } from "react-native-gesture-handler";
 
-const TabButton: React.FC<TabButtonProps> = ({
-	authAction,
-	children,
-	...props
-}) => (
-	<TabTrigger {...props} name={authAction} href={`/${authAction}`} asChild>
-		<AuthButton
-			className={cn(
-				"aspect-[5/2] h-auto flex-1 transform-gpu text-center shadow-2xl active:-translate-y-1 active:scale-[0.98] active:shadow-xl",
-				{
-					"shadow-brand-primary": authAction === "login",
-					"shadow-brand-secondary": authAction === "signup",
-				},
-			)}
-			authAction={authAction}>
-			{children}
-		</AuthButton>
-	</TabTrigger>
-);
-
-export default function AuthTabsLayout() {
+export default function AuthLayout() {
 	return (
-		<BoxScreen asChild>
-			<Tabs className="relative !pr-0">
-				<Box className="flex-1 pb-24">
-					<TabSlot />
-				</Box>
+		<BoxScreen
+			className="relative items-center justify-end gap-12"
+			style={{
+				paddingBottom: BOX_SCREEN_PADDING * 2,
+			}}>
+			{/* background image */}
+			<Box className="absolute inset-0">
+				<Image
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+					source={require("@/assets/images/auth-bg.webp")}
+					alt="minimalist 3d render dark background"
+					className="absolute inset-0 object-cover"
+				/>
+			</Box>
 
-				<TabList
-					className="absolute justify-between gap-6 !pr-0"
-					style={{
-						bottom: BOX_SCREEN_PADDING,
-						left: BOX_SCREEN_PADDING,
-					}}>
-					<TabButton authAction="signup">Sign up</TabButton>
-					<TabButton authAction="login">Log in</TabButton>
+			<AppLogoWithText />
 
-					{/* 
-                TODO: As this is debug workaround for the issue - TabTrigger not
-                working when wrapped in a custom component (TabButton), remove this when the reported GitHub issue on Expo repo is fixed.
-                For now, I'll remove this when I'm done with this component.
-                 */}
-					<TabTrigger name="signup" href={"/signup"} />
-					<TabTrigger name="login" href={"/login"} />
-				</TabList>
-			</Tabs>
+			<Box className="relative z-50 max-h-[80%] w-full">
+				<BlurredGradientBg />
+				<ScrollView>
+					<Slot />
+				</ScrollView>
+			</Box>
 		</BoxScreen>
 	);
 }
