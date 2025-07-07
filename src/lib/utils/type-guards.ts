4a7cd9af04e7afc32e955c1bea5f5d31ad5b7381
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-07 03:37:21 +0100

    refactor: export plainobject type

--- src/lib/utils/type-guards.ts
+++ src/lib/utils/type-guards.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-type PlainObject = Record<PropertyKey, unknown>;
+export type PlainObject = Record<PropertyKey, unknown>;
 
 export const isObject = (object: unknown): object is PlainObject =>
 	!!object && typeof object === "object" && !Array.isArray(object);
