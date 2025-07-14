4dc4c14d6d61f2fd20b85735a79917adf547873c
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-14 08:35:04 +0100

    refactor: Implement constants file for api routes

---
+++ src/app-colocation/api/constants.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+const API_BASE = "/api";
+
+// Google Auth APIs
+const GOOGLE_AUTH_API_BASE = `${API_BASE}/auth/google`;
+
+export const GOOGLE_AUTH_AUTHORIZE_PATH = `${GOOGLE_AUTH_API_BASE}/authorize`;
+export const GOOGLE_AUTH_CALLBACK_PATH = `${GOOGLE_AUTH_API_BASE}/callback`;
