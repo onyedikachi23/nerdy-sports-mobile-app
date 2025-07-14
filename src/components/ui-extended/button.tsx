4154abb9ecbc954c2aa98df7fce994bc5f3d1fc1
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-14 15:44:00 +0100

    refactor: Implement extended version of the gluestack buttom comp.

---
+++ src/components/ui-extended/button.tsx
@@ -0,0 +1,43 @@
+/** @format */
+
+import type { ReadableProps } from "@/types";
+import {
+	Button as GSButton,
+	ButtonText,
+	ButtonSpinner,
+	ButtonIcon,
+	ButtonGroup,
+} from "../ui/button";
+import type React from "react";
+import type { Pressable } from "react-native";
+
+type GSButtonProps = ReadableProps<typeof GSButton>;
+interface ButtonProps extends SafeOmit<GSButtonProps, "isDisabled" | "ref"> {
+	ref: React.Ref<typeof Pressable>;
+}
+
+const Button: React.FC<ButtonProps> = ({ disabled, ref, ...props }) => {
+	return (
+		<GSButton
+			{...props}
+			ref={ref as GSButtonProps["ref"]}
+			disabled={disabled}
+			isDisabled={disabled ? true : false}
+		/>
+	);
+};
+
+export { Button, ButtonText, ButtonSpinner, ButtonIcon, ButtonGroup };
+
+type ButtonTextProps = ReadableProps<typeof ButtonText>;
+type ButtonSpinnerProps = ReadableProps<typeof ButtonSpinner>;
+type ButtonIconProps = ReadableProps<typeof ButtonIcon>;
+type ButtonGroupProps = ReadableProps<typeof ButtonGroup>;
+
+export type {
+	ButtonProps,
+	ButtonTextProps,
+	ButtonSpinnerProps,
+	ButtonIconProps,
+	ButtonGroupProps,
+};
