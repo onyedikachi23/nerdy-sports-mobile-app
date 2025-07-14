14e709f68ff6afe6f9e66638837c5a8cfd6875f6
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-14 09:51:47 +0100

    [constanst]: Add constanst for env variables and for google auth

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,12 @@
 /** @format */
 
 export const BRAND_NAME = "Nerdy Sports";
+
+// Google OAuth Constants
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+export const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+
+// Environment Constants
+export const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL!;
+export const APP_SCHEME = process.env.EXPO_PUBLIC_SCHEME!;
